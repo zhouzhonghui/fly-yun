66d2ed38acdb1636749c39728befa0a3da47642f
zhouzhonghui <zhouzhonghui821@163.com>
2018-11-29 14:22:50 +0800

    针对游戏平台改造

--- fly-yun/fly-yun-config/src/main/java/cn/fly/yun/config/filter/RequestFilter.java
+++ fly-yun/fly-yun-config/src/main/java/cn/fly/yun/config/filter/RequestFilter.java
@@ -3,6 +3,7 @@ package cn.fly.yun.config.filter;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
@@ -14,6 +15,10 @@ public class RequestFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse httpResponse = (HttpServletResponse)servletResponse;
+        httpResponse.setHeader("Access-Control-Allow-Headers","Content-Type,M-Client-ID,M-Timestamp,M-Request-ID,M-Request-Signature");
+        httpResponse.setHeader("Access-Control-Allow-Credentials", "true");
+        httpResponse.addHeader("Access-Control-Allow-Origin", "*");
         filterChain.doFilter(servletRequest,servletResponse);
     }
 
